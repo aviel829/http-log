54ef40377fec2d88ef8123c6f158c4a356e8c383
aviel829 <aviel829@gmail.com>
2015-10-06 14:04:38 +0300

    ``

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http');
 var server = http.createServer(app);
+var request = require('request');
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
@@ -13,12 +14,28 @@ app.get('/api/report/', function (request, response) {
     response.json(cached_data);
 });
 
-app.post('/api/report/', function (request, response) {
-    var data = request.body;
-    cached_data.push(data);
-    console.log(data);
+app.post('/api/report/', function (req, res) {
+    var data = req.body;
+    var ip = data.ip;
+    var url = 'https://freegeoip.net/json/'+ip;
 
-    response.send("ok, thank you");
+    request(url, function (error, response, body) {
+        if (error || response.statusCode != 200) {
+            console.log('failed');
+            res.send("failed");
+            return;
+        }
+
+        var geoipData = JSON.parse(body);
+        data.country = geoipData.country_name;
+        data.region = geoipData.region_name;
+
+        cached_data.push(data);
+        console.log(data);
+
+        res.send("success");
+    });
+
 });
 
 app.get('/', function (request, response) {
